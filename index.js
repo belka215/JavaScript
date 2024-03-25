c7a984ff46108c9321aaf2237ff9b1c7dd9e1a23
Izabella Novikova <belka215@gmail.com>
2024-03-25 22:51:42 +0100

    feat: solutions

---
+++ index.js
@@ -0,0 +1,95 @@
+"use strict";
+
+// Задача 1.
+
+// let x = 20;
+// let y = 58;
+// let z = 42;
+
+// console.log(x + y + z);
+
+
+// Задача 2.
+
+// const sec = 60;
+// const min = 60;
+// const hour = 24;
+// const day = 365;
+
+// const myAgeInSeconds = 25 * day * hour * min * sec;
+// console.log(myAgeInSeconds);
+
+
+// Задача 3.
+
+// let count = 42;
+// let userName = '42';
+
+// // const countToString = count.toString();
+// const countToString = String(count);
+
+// // const userNameToNumber = Number(userName);
+// const userNameToNumber = +userName;
+
+// console.log(countToString, userNameToNumber);
+
+
+// Задача 4.
+
+// let a = 1;
+// let b = 2;
+// let c = 'белых медведей';
+
+// console.log(`${a}${b} ${c}`);
+
+
+// Задача 5.
+
+// const word1 = 'доступ';
+// const word2 = 'морпех';
+// const word3 = 'наледь';
+// const word4 = 'попрек';
+// const word5 = 'рубило';
+
+// const lengthWords = word1.length + word2.length + word3.length + word4.length + word5.length;
+// console.log(lengthWords);
+
+
+// Задача 6.
+
+// const string = 'Hello';
+// const number = 123;
+// const sum = null;
+
+// console.log("Variable string have type: " + typeof string);
+// console.log("Variable number have type: " + typeof number);
+// console.log("Variable sum have type: " + typeof sum);
+
+
+// Задача 7.
+
+// const name = prompt('What is your name?');
+// const age = prompt('How old are you?');
+
+// console.log(name, age);
+
+
+// Задача 8.
+
+// let a = 4;
+// let b = 3;
+
+// [a, b] = [b, a];
+
+// console.log(a, b);
+
+
+// Задача 9.
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!"
+
+console.log(codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1]);
